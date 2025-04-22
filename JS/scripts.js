e3d96e2d4da7131aa58ac73cc756b694a1105ab0
James Morton <jamesmorton@Jamess-MacBook-Pro.local>
2025-04-22 10:10:12 +0100

    added the buttons to pause and play animation and finsihed all aimations

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,9 @@
 // Exercise 12: Write the JS to target the .stop-button class on click. Toggle the stop-animations class on and off
+
+function myPlayFunction() {
+    document.getElementById("animate-me").style.animationPlayState = "running";
+  }
+  
+  function myPauseFunction() {
+    document.getElementById("animate-me").style.animationPlayState = "paused";
+  }
